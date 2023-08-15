39617af72de30cb1547939b7248793b69435122b
楊宜倫 <87465975+Yilun0823@users.noreply.github.com>
2023-08-15 15:52:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def interpret_bmi(bmi):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        weight = float(request.form['weight'])
         height = float(request.form['height'])
+        weight = float(request.form['weight'])
         bmi = calculate_bmi(weight, height)
         interpretation = interpret_bmi(bmi)
         return render_template('result.html', bmi=bmi, interpretation=interpretation)
